ac1776f48f9f4b865e973662248961cc798bdc41
Vaughn Haynes <drbrounsuga@gmail.com>
2017-06-04 21:53:07 -0500

    updated test files and write settings

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let ep = new exiftool.ExiftoolProcess('./src/assets/exiftool');
 //verify request below. Cant pass main to renderer
 ipcMain.on('exiftool-write', (event, filePath, data, indx) => {
   ep.open()
-    .then(() => ep.writeMetadata(filePath, data, ['overwrite_original']))
+    .then(() => ep.writeMetadata(filePath, data, ['ignoreMinorErrors','preserve','overwrite_original']))
     .then((res) => {
       event.sender.send('exiftool-write-reply', res, indx);
     })
